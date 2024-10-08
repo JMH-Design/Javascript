c616306a92ff05b7719da0594b2ec2f73f7cadf7
JMH-Design <jmhaggit@gmail.com>
2024-10-07 22:35:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,123 +1,64 @@
-const firstName = "Jared";
-const job = "Product Designer";
-const birthYear = 1992;
-const currentYear = 2037;
+'use strict';
 
-const jared =
-  "I'm" + " " + firstName + " " + "a" + " " + (currentYear - birthYear);
-console.log(jared);
+let hasDriversLicense = false;
+const passTest = true;
 
-const jaredNew = `I'm ${firstName}, a ${job} , born in ${birthYear} but currently live in ${currentYear}`;
-console.log(jaredNew);
+if (passTest) hasDriverLicense = true;
+If(hasDriversLicense);
+console.log('Sample');
 
-const age = 15;
-if (age >= 18) {
-  console.log("sarah can start driving license 😍");
-} else {
-  const yearsLeft = 18 - age;
-  console.log(`Sarah is too young. Wait another ${yearsLeft}`);
+//Functions
+function logger() {
+  console.log('My name is Jared');
 }
-const birthYear = 2001;
-let century;
-if (birthYear <= 2000) {
-  century = "20th";
-} else {
-  century = "21st";
+// Calling/running/invoking the fuction
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
 }
-console.log(century);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMIMark, BMIJohn);
-
-let higherBMI;
-if (BMIJohn > BMIMark) {
-  higherBMI = "Johns BMI is higher than Marks";
-} else {
-  higherBMI = "Marks BMI is higher than Johns";
+function gradeInClass(homework, exams) {
+  const score = (homework + exams) / 2;
+  return score;
 }
-console.log(higherBMI);
+const firstExam = gradeInClass(87, 95);
+console.log(firstExam);
 
+const secondExam = gradeInClass(78, 90);
+console.log(secondExam);
 
-let higherBMI;
-if (BMIJohn > BMIMark) {
-  higherBMI = `Johns BMI ${BMIJohn} is higher than Marks`;
-} else {
-  higherBMI = `Marks BMI of ${BMIMark} is higher than Johns BMI of ${BMIJohn}`;
+function courseScore(secondExam, firstExam) {
+  const classScore = (firstExam + secondExam) / 2;
+  return classScore;
 }
-console.log(higherBMI);
-let fatJohn;
-if (BMIJohn > BMIMark) {
-  fatJohn = "John is so fat";
-} else {
-  fatJohn = "Mark is so fat";
+const sample = courseScore(90, 56);
+console.log(sample);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
-console.log(fatJohn);
+const age1 = calcAge1(1992);
+console.log(age1);
 
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1998);
+console.log(age1, age2);
 
-// type conversion
+const age3 = (birthYear) => 2037 - birthYear;
 
-const inputYear = "1991";
-const z = Number(inputYear);
-console.log(Number(inputYear));
-console.log(z + 18);
-
-//Type Coercion
-
-const money = 100;
-if (money) {
-  console.log("Dont spend it all");
-} else {
-  console.log("you should get a job");
-}
-let height = 1;
-if (height > 2) {
-  console.log("YAY height");
-} else {
-  console.log("NArrrr height");
-}
-
-const age = 18;
-if (age === 18) {
-  console.log("Welcome to adulthood Jackass");
-} else {
-  console.log("Stay here, as long as you can");
-}
-
-const favorite = Number(prompt("What is your favorite number?"));
-console.log(favorite);
-console.log(typeof favorite);
-
-if (favorite == 8) {
-  console.log("cool, 8 is an amazing number");
-} else if (favorite > 8) {
-  console.log("you picked the wrong number bud");
-} else if (favorite < 7) {
-  console.log("Terrible number");
-} else {
-  console.log("alksdjflaksdjf");
-}
-if (favorite !== 8) {
-  console.log("to be determined");
-}
-*/
-const hasDriversLicense = true;
-const hasGoodVision = false;
-
-console.log(hasDriversLicense && hasGoodVision);
-console.log(hasDriversLicense || hasGoodVision);
-console.log(!hasGoodVision);
-
-const shouldDrive = hasDriversLicense && hasGoodVision;
-if (shouldDrive) {
-  console.log("Sarah is able to drive");
-} else {
-  console.log("Someone else should drive");
-}
-const isTired = false;
-console.log((hasDriversLicense && hasGoodVision) || isTired);
+const yearsUntilRetirement = (birthYear) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+console.log(yearsUntilRetirement(1878));
